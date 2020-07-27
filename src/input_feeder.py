629c7b9d69108b84948cf38a50e22de3561f9387
Ileriayo Adebiyi <ileriayoadebiyi@gmail.com>
2020-07-27 11:50:41 +0100

    Bug: Handle input file type

--- src/input_feeder.py
+++ src/input_feeder.py
@@ -7,34 +7,33 @@ Sample usage:
         do_something(batch)
     feed.close()
 '''
+import os
 import logging as log
 import cv2
 from numpy import ndarray
 
 class InputFeeder:
-    def __init__(self, input_type=None, input_file=None):
+    def __init__(self, input_file, input_type=None):
         '''
         input_type: str, The type of input. Can be 'video' for video file, 'image' for image file,
                     or 'cam' to use webcam feed.
         input_file: str, The file that contains the input image or video file. Leave empty for cam input_type.
         '''
-        self.input_type=input_type
-        if input_type=='video' or input_type=='image':
-            self.input_file=input_file
-    
+        self.input_file = input_file
+        self.input_type = input_type
+        self.get_input_type()
+        
     def get_input_type(self):
         img_extension = ['.png', '.bmp', '.jpg', '.jpeg', 'tif']
         vid_extension = ['.mp4']
         if self.input_file.lower() == 'cam':
             self.input_type = 'cam'
-        elif input_file.split('.')[-1].lower() in img_extension:
+        elif os.path.splitext(self.input_file)[-1] in img_extension:
             self.input_type = 'image'
-        elif input_file.split('.')[-1].lower() in vid_extension:
+        elif os.path.splitext(self.input_file)[-1] in vid_extension:
             self.input_type = 'video'
-        return
 
     def load_data(self):
-        self.get_input_type()
         if self.input_type=='video':
             self.cap=cv2.VideoCapture(self.input_file)
         elif self.input_type=='cam':
